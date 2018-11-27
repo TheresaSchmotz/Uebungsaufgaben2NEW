c37ef3b6b1bbee36acfaf56d68ec1e045e254ddd
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-27 03:04:28 +0100

    Aufgabe 5 ich kapituliere

---
+++ Aufgabe5/src/Aufgabe5_1810653557.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Aufgabe5_1810653557 {
+    public static void main(String[]args){
+
+        void forschleife(int a){
+            for(int t = 0; t <= a; t++){
+                System.out.println("Ausgabe =" + t);
+            }
+        }
+
+        void whileschleife(int a){
+
+        }
+
+        void nutzerschleife(){
+            int a =Integer.parseInt(JOptionPane.showInputDialog("Bitte Zahl a eingeben"));
+            int b =Integer.parseInt(JOptionPane.showInputDialog("Bitte Zahl b eingeben"));
+
+
+        }
+    }
+}
