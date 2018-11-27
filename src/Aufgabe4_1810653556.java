97a83f942271944968eb7a8acb6e3340f1b5e1c9
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-27 02:48:17 +0100

    Aufgabe 4 soll das sein LOL

---
+++ src/Aufgabe4_1810653556.java
@@ -0,0 +1,9 @@
+public class Aufgabe4_1810653556 {
+    public static void main(String[]args){
+            while (true){
+                for(int zahl=1; zahl <11; zahl ++){
+                    System.out.println("Der Schleifenzähler ist : " + zahl);
+                }
+        }
+    }
+}
