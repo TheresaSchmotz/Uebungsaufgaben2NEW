9619b2044a99fc40022d6d7594b6348784584dc9
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-27 02:36:02 +0100

    Aufgabe 3 somehow rip

---
+++ src/Aufgabe3_1810653557.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Aufgabe3_1810653557 {
+    public static void main(String[]args){
+
+        double alter;
+        double eingabe = Double.parseDouble(JOptionPane.showInputDialog("Bitte geben Sie ein"));
+        JOptionPane.showMessageDialog(null, eingabe);
+
+        int hausnummer = Integer.parseInt(JOptionPane.showInputDialog("Bitte geben Sie ein"));
+        boolean wahrheit = Boolean.parseBoolean(JOptionPane.showInputDialog("Bitte geben Sie ein"));
+        long zahl = Long.parseLong(JOptionPane.showInputDialog("Bitte geben Sie ein"));
+        char zahl2 = JOptionPane.showInputDialog("Bitte geben Sie ein").charAt(0);
+        byte zahl3 = Byte.parseByte(JOptionPane.showInputDialog("Bitte geben Sie ein"));
+        short zahl4 = Short.parseShort(JOptionPane.showInputDialog("Bitte geben Sie ein"));
+        float zahl5 = Float.parseFloat(JOptionPane.showInputDialog("Bitte geben Sie ein"));
+
+         double String.valueOf(alter) = alter;
+    }
+
+}
